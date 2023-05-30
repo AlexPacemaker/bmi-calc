3c045ef107e51d69633ce349ee38cd08db06bdb9
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-05-30 16:28:14 +0300

    store type added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,7 @@
 // Эта функция создает Redux-хранилище с помощью redux-toolkit и настраивает его для использования с reducers. В данном случае, она создает хранилище с единственным reducer'om - bmiSlice. Это означает, что действия, отправленные в хранилище, будут обрабатываться только этим reducer'ом.
 
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 import bmiSlice from "./slices/bmiSlice";
 
 export const store = configureStore({
@@ -8,3 +9,7 @@ export const store = configureStore({
     bmiSlice,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
